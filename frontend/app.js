3f464323106d65b6ecba16e5f74cf8e71dda2e0b
“Luis <l.garcia24@hotmail.com>
2020-09-10 18:21:51 -0500

    added hover effect on menu

--- frontend/app.js
+++ frontend/app.js
@@ -1,28 +1,5 @@
 const main = document.querySelector("main");
 
-function dynamicsort(property, order) {
-  let sort_order = 1;
-  if (order === "desc") {
-    sort_order = -1;
-  }
-  return function (a, b) {
-    // a should come before b in the sorted order
-    if (a[property] < b[property]) {
-      return -1 * sort_order;
-      // a should come after b in the sorted order
-    } else if (a[property] > b[property]) {
-      return 1 * sort_order;
-      // a and b are the same
-    } else {
-      return 0 * sort_order;
-    }
-  };
-}
-
-fetch("http://localhost:3000/pokemon_cards")
-  .then((response) => response.json())
-  .then((cards) => cards.sort(dynamicsort("pokedexNumber", "desc")));
-
 fetch("http://localhost:3000/pokemon_cards")
   .then((response) => response.json())
   .then((cards) =>
